ac6e2c3e4c7d4371e701166670290bbb297df587
salehmuffarreh <salehmuffarreh@gmail.com>
2022-08-20 02:35:11 +0300

    cw7

---
+++ cw6/Assets/player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class player : MonoBehaviour
+{
+    public Transform min;
+    public Transform max;
+    public float inc;
+
+    public AudioSource src;
+    // Start is called before the first frame update
+    void Start()
+    {
+        src= GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x + inc, min.position.x, max.position.x), transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x - inc, min.position.x, max.position.x), transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(0);
+            src.Play();
+        }
+    }
+
+
+
+
+}
+
